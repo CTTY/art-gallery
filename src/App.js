b5866926391226f51792b7825c3940ca539e858f
Ming <lumingabby@outlook.com>
2019-08-16 19:35:43 -0400

    project page structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
+
+import Preloader from './Preloader/Preloader';
+import Header from './Header/Header';
+import Carousel from './Carousel/Carousel';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Preloader />
+      <Header />
+      <Carousel />
+      <script src="./js/jquery.min.js"></script>
+      <script src="./js/popper.min.js"></script>
+      <script src="./js/bootstrap.min.js"></script>
+      <script src="./js/mona.bundle.js"></script>
+      <script src="./js/default-assets/active.js"></script>
+    </React.Fragment>
+    
   );
 }
 
