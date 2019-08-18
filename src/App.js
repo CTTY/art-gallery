da995157653d6c349b9aea32c1a95c39764f5b5d
Ming <lumingabby@outlook.com>
2019-08-18 12:21:25 -0400

    fixed carousel format

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './style.css';
 
+import Carousel from './Carousel/Carousel';
 import Preloader from './Preloader/Preloader';
 import Header from './Header/Header';
 
 function App() {
   return (
     <React.Fragment>
-      <Preloader />
+      <Carousel />
+      {/* <Preloader />
       <Header />
       <script src="./js/jquery.min.js"></script>
       <script src="./js/popper.min.js"></script>
       <script src="./js/bootstrap.min.js"></script>
       <script src="./js/mona.bundle.js"></script>
-      <script src="./js/default-assets/active.js"></script>
+      <script src="./js/default-assets/active.js"></script> */}
     </React.Fragment>
     
   );
--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,46 +1,106 @@
-import React, {Component} from 'react';
-import '../style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import RBCarousel from "react-bootstrap-carousel";
+import "../style.css";
 
-class Carousel extends Component(){
-    render(){
-        return(
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="../img/bg-img/1.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="./img/bg-img/2.jpg"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="./img/bg-img/19.jpg"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        );
-    }
+// Import Bootstrap Components
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const styles = { height: 400, width: "100%" };
+
+class Carousel extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoplay: true
+    };
+  }
+  
+  render() {
+    let { leftIcon, rightIcon } = this.state;
+    return (
+    <div class="mona-projects-area section-padding-80-0 mb-50">
+    <Container>
+        <Row style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+        <Col md={18} md={9}>
+            <RBCarousel
+            animation={true}
+            autoplay={this.state.autoplay}
+            slideshowSpeed={2000}
+            leftIcon={leftIcon}
+            rightIcon={rightIcon}
+            onSelect={this.onSelect}
+            ref={r => (this.slider = r)}
+            version={4}
+            >
+            {/* <Media>
+                <img
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src="https://www.w3schools.com/bootstrap/ny.jpg"
+                    alt="Generic placeholder"
+                />
+                <Media.Body>
+                    <h5>Media Heading</h5>
+                    <p>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                    tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+                    Donec lacinia congue felis in faucibus.
+                    </p>
+                </Media.Body>
+            </Media> */}
+            <div className="carousel-item active">
+                <img src="https://www.w3schools.com/bootstrap/ny.jpg" className="d-block w-100" alt="..." />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div className="carousel-item active">
+                <img src="https://pbs.twimg.com/profile_images/1147673817537490944/FfVoY021_400x400.jpg" className="d-block w-100" alt="..." />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div className="carousel-item active">
+                <img src="https://data.whicdn.com/images/274547293/superthumb.jpg" className="d-block w-100" alt="..." />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            {/* <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                <span>text</span>
+                <div className="carousel-caption">Text</div>
+            </div> */}
+            {/* <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                <div className="carousel-center">
+                <iframe
+                    style={{ width: 500 }}
+                    height="250"
+                    src="https://www.youtube.com/embed/MhkGQAoc7bc?showinfo=0"
+                    frameBorder="0"
+                    allowFullScreen
+                />
+                </div>
+                <div className="carousel-caption">Youtube</div>
+            </div> */}
+            </RBCarousel>
+        </Col>
+        </Row>
+
+    </Container>
+</div>
+      
+    );
+  }
 }
 
+// ReactDOM.render(<Demo />, document.getElementById("Demo"));
 export default Carousel;
